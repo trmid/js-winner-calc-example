6b405c44431956c8666cada3571d4872f2e1b305
Trevor Richard <40277611+trmid@users.noreply.github.com>
2024-07-05 15:42:49 -0400

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+![SSS Logo](docs/favicon.svg)
+
+# SSS: Svelte Static SPA
+A template repository for building a single page web app (SPA) with Svelte.
+
+## Configure
+
+Change app options in `sss.config.json`.
+
+## Development
+
+Run `npm run dev` to start the development server. Edit the svelte and typescript files in the `src` folder to make changes to your app. Change the `docs/index.html` file directly to make changes to your app's metadata and to add additional stylesheets or resource links.
+
+## Build
+
+When your app is production ready, run the following command to output the static site bundle to the `docs/bundle` directory:
+
+### Linux
+
+`npm run build`
+
+### Windows
+
+`npm run build-win`
+
+## Publish
+
+After building your bundle, publish the static files in the `docs` directory on the platform of your choice, or if your build directory is named `docs`, you can host them directly on github by enabling github pages to serve directly from the `docs` folder in your repository.
---
+++ src/sw.template.js
@@ -0,0 +1,54 @@
+const cacheName = `$PACKAGE_VERSION`; // Change value to force update
+const essentialFilesToCache = `$ESSENTIAL_FILES_TO_CACHE`; // Generated filenames as string array
+const secondaryFilesToCache = `$SECONDARY_FILES_TO_CACHE`; // Generated filenames as string array
+
+self.addEventListener("install", event => {
+
+	// Delete any non-current cache
+	event.waitUntil(
+		caches.keys().then(keys => 
+			Promise.all(
+				keys.map(key => {
+					if (![cacheName].includes(key)) {
+						return caches.delete(key);
+					}
+				})
+			)
+		)
+	);
+	
+	// Kick out the old service worker
+	self.skipWaiting();
+
+  // Wait for essential file installation:
+	event.waitUntil(
+		caches.open(cacheName).then(cache => {
+			return cache.addAll(JSON.parse(essentialFilesToCache));
+		})
+	);
+
+  // Trigger async, non-essential file installation:
+  caches.open(cacheName).then(cache => {
+    return Promise.allSettled(
+      JSON.parse(secondaryFilesToCache).map(file => cache.add(file).catch(console.warn))
+    );
+  }).catch(console.error);
+
+});
+
+// Get data on screen as quickly as possible, then update once the network has returned the latest data. 
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.open(cacheName).then(cache => 
+			cache.match(event.request).then(response => {
+        const networkResponse = fetch(event.request).then(networkResponse => {
+					if(event.request.method === "GET" && Math.floor(networkResponse.status / 100) == 2) {
+						cache.put(event.request, networkResponse.clone());
+					}
+					return networkResponse;
+				}).catch(console.warn);
+				return response ?? networkResponse;
+			})
+		)
+	);
+});
